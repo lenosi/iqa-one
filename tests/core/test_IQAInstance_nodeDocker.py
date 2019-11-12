3cd840e5af5f777864299f6ea0c0f14d361ab357
Dominik Vecera <dvecera@redhat.com>
2019-11-12 17:00:03 +0100

    Static typing of "tests" folder and added max-line-length=120 to tox.ini

--- tests/core/test_IQAInstance_nodeDocker.py
+++ tests/core/test_IQAInstance_nodeDocker.py
@@ -1,38 +1,39 @@
 import pytest
 
 from iqa.instance.instance import Instance
+from iqa.system.node import Node
 from iqa.system.node.node_docker import NodeDocker
 from iqa.system.executor.executor_container import ExecutorContainer
 
 
 class TestInstanceNodeDocker:
-    iqa_instance = Instance()
+    iqa_instance: Instance = Instance()
 
     @pytest.fixture
-    def instance(self):
+    def instance(self) -> Instance:
         return TestInstanceNodeDocker.iqa_instance
 
     @pytest.fixture
-    def node(self, instance):
+    def node(self, instance: Instance) -> Node:
 
-        executor = ExecutorContainer(
+        executor: ExecutorContainer = ExecutorContainer(
             name="Docker executor",
             container_name='sshd-iqa'
         )
 
-        example_node = NodeDocker(hostname="sshd-iqa", executor=executor)
+        example_node: NodeDocker = NodeDocker(hostname="sshd-iqa", executor=executor)
         instance.nodes.append(example_node)
 
-        node: NodeDocker = instance.nodes[0]
+        node: Node = instance.nodes[0]
 
         return node
 
-    def test_ping(self, node):
-        node_ping = node.ping()
+    def test_ping(self, node: NodeDocker) -> None:
+        node_ping: bool = node.ping()
 
         assert node_ping
 
-    def test_get_ip(self, node):
-        node_ip = node.get_ip()
+    def test_get_ip(self, node: NodeDocker) -> None:
+        node_ip: str = node.get_ip()
 
         assert node_ip is not None
--- tests/system/executors/test_executorSsh.py
+++ tests/system/executors/test_executorSsh.py
@@ -1,24 +1,25 @@
 from subprocess import check_output
 
+from iqa.system.executor.execution import Execution
 from iqa.system.executor.executor_ssh import ExecutorSsh
 from iqa.system.command.command_base import Command
 
 
 class TestExecutorSsh:
-    def test_execute(self):
-        ip = check_output(
+    def test_execute(self) -> None:
+        ip: str = check_output(
             ['docker', 'inspect', '-f', '"{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}"',
              'sshd-iqa'], encoding='utf-8').strip('\n\"')
 
-        executor = ExecutorSsh(
+        executor: ExecutorSsh = ExecutorSsh(
             user="root",
             hostname=ip,
             name="SSH executor",
             ssl_private_key="tests/images/sshd_image/identity"
         )
 
-        cmd = Command(args=["whoami"])
+        cmd: Command = Command(args=["whoami"])
 
-        execution = executor.execute(cmd)
+        execution: Execution = executor.execute(cmd)
 
         assert execution.completed_successfully()
