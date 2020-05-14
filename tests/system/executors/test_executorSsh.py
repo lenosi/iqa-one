7ee3c7cafc6ec339114c83b7e618c9d46823ba12
Dominik Lenoch <dlenoch@redhat.com>
2020-05-14 13:27:05 +0200

    Add executor ssh test

--- tests/system/executors/test_executorSsh.py
+++ tests/system/executors/test_executorSsh.py
@@ -1,29 +1,78 @@
-import pytest
-import logging
+import nest_asyncio
 
-from iqa.system.executor.execution import Execution
-from iqa.system.executor.executor_ssh import ExecutorSsh
+import logging
+import sys
+
+import asyncssh
+import pytest
+
 from iqa.system.command.command_base import Command
+from iqa.system.executor import Execution, ExecutorSsh
+from iqa.system.executor.ssh import ExecutorSshOld
 from iqa.system.node import NodeDocker
 
-logging.basicConfig(level=logging.DEBUG)
+from asyncio.events import AbstractEventLoop
+
+from iqa.utils.tcp_util import wait_host_port
+
+
+async def run_client(ip):
+    await wait_host_port(ip, port=22)
+    async with asyncssh.connect(
+            ip,
+            username='root',
+            password='SomeSecretPassword0987',
+            known_hosts=None,
+            client_keys=None
+            # client_keys=[key]
+            ) as conn:
+        result = await conn.run('ls', check=True)
+        print(result.stdout, end='')
 
 
 class TestExecutorSsh:
 
+    # @pytest.fixture
+    # def executor(self, node: NodeDocker) -> ExecutorSshOld:
+    #     executor: ExecutorSshOld = ExecutorSshOld(
+    #         name="Docker executor",
+    #         hostname=node.ip,
+    #         ssl_private_key="../../devel/images/centos8-init-sshd/identity"
+    #     )
+    #     return executor
+
     @pytest.fixture
-    def executor(self, docker_services, node: NodeDocker) -> ExecutorSsh:
+    def executor(self, node: NodeDocker) -> ExecutorSsh:
         executor: ExecutorSsh = ExecutorSsh(
             name="Docker executor",
-            hostname=node.ip,
-            ssl_private_key="../../devel/images/centos8-init-sshd/identity"
+            user='root',
+            ip=node.ip,
+            password='SomeSecretPassword0987',
+            known_hosts=None,
+            client_keys=None
         )
         return executor
 
-    def test_execute(self, executor: ExecutorSsh) -> None:
-        cmd: Command = Command(args=["whoami"], stdout=True)
+    @pytest.mark.asyncio
+    async def test_evloop(self, event_loop: AbstractEventLoop, node: NodeDocker):
+        # await asyncio.sleep(0, loop=event_loop)
 
-        execution: Execution = executor.execute(cmd)
-        execution.wait()
-        assert execution.completed_successfully()
-        assert execution.read_stdout().rstrip().lstrip() == "root"
+        nest_asyncio.apply()
+        try:
+            event_loop.run_until_complete(run_client(ip=node.ip))
+        except (OSError, asyncssh.Error) as exc:
+            sys.exit('SSH connection failed: ' + str(exc))
+        logging.basicConfig(level=logging.DEBUG)
+
+    @pytest.mark.asyncio
+    async def test_asyncssh_session(self, event_loop: AbstractEventLoop, node: NodeDocker, executor: ExecutorSsh):
+        session = await executor.new_session()
+
+    #
+    # def test_execute(self, executor: ExecutorSshOld, event_loop: AbstractEventLoop) -> None:
+    #     cmd: Command = Command(args=["whoami"], stdout=True)
+    #
+    #     execution: Execution = executor.execute(cmd)
+    #     execution.wait()
+    #     assert execution.completed_successfully()
+    #     assert execution.read_stdout().rstrip().lstrip() == "root"
